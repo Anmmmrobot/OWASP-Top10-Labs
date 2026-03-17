49d5c49141f6a060673a329ea7e31f6913cf04d6
Anmmmrobot <Tamashigarybay@gmail.com>
2026-03-17 14:05:59 +0800

    Update app.py

--- A08_Software_and_Data_Integrity_Failures/environment/app/app.py
+++ A08_Software_and_Data_Integrity_Failures/environment/app/app.py
@@ -1,1 +1,24 @@
+from flask import Flask, request
+import pickle
+import base64
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "A08 Vulnerable App Running"
+
+# 漏洞点：不安全反序列化
+@app.route("/deserialize", methods=["POST"])
+def deserialize():
+    data = request.form.get("data")
+
+    try:
+        decoded = base64.b64decode(data)
+        obj = pickle.loads(decoded)   # 漏洞核心,用户可以控制服务器执行任意 Python 对象,本质是远程代码执行 RCE 入口
+        return f"Deserialized: {obj}"
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
